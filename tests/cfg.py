26e37e1630707e9fae03c365a5a89727133d8397
urja <urjasrivastava123@gmail.com>
2021-11-23 00:08:36 +0530

    added cfg

---
+++ tests/cfg.py
@@ -0,0 +1,4 @@
+from staticfg import CFGBuilder
+
+cfg = CFGBuilder().build_from_file('cfg', r'C:\Users\urja\Desktop\Control-Flow-Software-Testing-in-Python\src\Chess.py')
+cfg.build_visual('cfg', 'png')
